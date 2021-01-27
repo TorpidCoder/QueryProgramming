a8a0064b3df3ad2b2e96372979cf11888405a524
sahilthenagpal <thisissahilnagpal@gmail.com>
2021-01-27 00:32:51 -0500

    query

--- AdvanceSQL/Assignments/Assignments.sql
+++ AdvanceSQL/Assignments/Assignments.sql
@@ -15,3 +15,23 @@ group by
     3
 order by
 	4 desc;
+    
+# Assignment-2
+
+select
+	count(distinct(website_sessions.website_session_id)) as Sessions,
+    count(distinct(orders.order_id)) as Orders,
+    count(distinct(orders.order_id))/count(distinct(website_sessions.website_session_id)) as sessions_to_order_conv_rate
+from
+	website_sessions
+    LEFT JOIN
+    orders
+    ON
+    website_sessions.website_session_id = orders.website_session_id
+where
+	website_sessions.created_at < '2012-04-14' AND
+    website_sessions.utm_source = 'gsearch' AND
+    website_sessions.utm_campaign = 'nonbrand';
+
+    
+
