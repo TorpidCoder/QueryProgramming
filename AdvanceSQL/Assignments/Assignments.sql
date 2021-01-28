0d23e245e4004c675f746cb99f334552eda8b7c3
sahilthenagpal <thisissahilnagpal@gmail.com>
2021-01-27 23:41:10 -0500

    new queries

--- AdvanceSQL/Assignments/Assignments.sql
+++ AdvanceSQL/Assignments/Assignments.sql
@@ -33,5 +33,48 @@ where
     website_sessions.utm_source = 'gsearch' AND
     website_sessions.utm_campaign = 'nonbrand';
 
+# Assignment-3
+select * from mavenfuzzyfactory.website_sessions limit 10;
+
+
+
+select 
+	MIN(DATE(created_at)) as week_start_date,
+    count(distinct(website_session_id)) as sessions
+from 
+	mavenfuzzyfactory.website_sessions
+where
+	website_sessions.created_at < '2012-05-12' AND
+    website_sessions.utm_source = 'gsearch' AND
+    website_sessions.utm_campaign = 'nonbrand'
+group by
+	week(created_at),
+    year(created_at);
     
+# Assignment -4
+
+select 
+	device_type,
+    count(distinct(website_sessions.website_session_id)) as sessions,
+    count(distinct(order_id)) as orders,
+    count(distinct(order_id))/count(distinct(website_sessions.website_session_id)) as session_to_order_conv_rate
+from 
+	mavenfuzzyfactory.website_sessions
+left join
+	mavenfuzzyfactory.orders
+ON
+	website_sessions.website_session_id = orders.website_session_id
+where
+	website_sessions.created_at < '2012-05-11' AND
+    website_sessions.utm_source = 'gsearch' AND
+    website_sessions.utm_campaign = 'nonbrand'
+group by
+	device_type;
+	
+
+show warnings;
+
+
+
+
 
